246625e1fc1f2c612df9781c87720a881dd6064e
Amnaf07 <168194296+Amnaf07@users.noreply.github.com>
2024-04-26 18:49:20 +0500

    Add files via upload

---
+++ add.php
@@ -0,0 +1,23 @@
+<html>
+
+<body>
+    <h1>Add, Subtract, Multiply, Divide</h1>
+</body>
+
+<?php
+
+$num1=3;
+$num2=4;
+$num3=$num1+$num2;
+echo"$num3 <br>";
+$num3=$num2-$num1;
+echo"$num3 <br>";
+$num3=$num1*$num2;
+echo"$num3 <br>";
+$num3=$num1/$num2;
+echo"$num3 <br>";
+
+
+
+?>
+</html>
