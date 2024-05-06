fe3bf4fa3fb759726c20f747ba1bb1c6c9325009
Syed Tajuddin Irfani <96831810+SyedTajuddinIrfani@users.noreply.github.com>
2024-05-06 22:51:23 +0500

    Update signup_process.php

--- signup_process.php
+++ signup_process.php
@@ -6,12 +6,16 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     $useremail = $_POST['useremail']; 
     $userpass = $_POST['userpasses'];
     $userpasscon = $_POST['userpassconfirm'];
+    if($userpass == $userpasscon){
     $query = "insert into registration(Name, Email, Password, ConfirmPassword) 
     values('$username','$useremail','$userpass','$userpasscon')";
     if ($conn->query($query) == true) {
         echo "Registration Successful";
     } else {
         echo $conn->error;
+    }
+    }else{
+        echo "Please Enter Password Correctly";
     }
 }
 ?>
